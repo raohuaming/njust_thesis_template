daaad0d82c2ae1bf9b34d2c5b85a1e230cd3c72d
Huaming Rao <huaming.rao@gmail.com>
2015-12-17 13:11:45 +0800

    init commit

---
+++ makemeTina.bat
@@ -0,0 +1,9 @@
+xelatex main.tex
+bibtex main
+copy /b fixbbl.txt+main.bbl  main2.bbl
+del main.bbl
+ren main2.bbl main.bbl
+xelatex main.tex
+xelatex main.tex
+del *.aux *.bbl *.blg *.out *.thm *.toc *.lof *.fen *.toe *.lot *.ten *.log *.gz *.gz(busy) *.synctex *.loa /s
+SumatraPDF -reuse-instance main.pdf
